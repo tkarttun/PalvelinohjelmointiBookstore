6a23fa093dd3f2321e2710bc6ec0fe12151c5c76
Toni Karttunen <toni.karttunen@gmail.com>
2021-09-05 17:06:03 +0300

    Alustus

---
+++ src/main/java/fi/karttunen/Bookstore/domain/Book.java
@@ -0,0 +1,12 @@
+package fi.karttunen.Bookstore.domain;
+
+public class Book {
+	private String title;
+	private String author;
+	private String year;
+	private String isbn;
+	private String price;
+
+	//Tarkista ominaisuuksien luokat ja muuta tarvittaessa
+	//Generoi lopuksi construktorit, getterit, setterit yms.
+}
